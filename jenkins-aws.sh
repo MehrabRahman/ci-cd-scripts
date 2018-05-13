96ebb8fb46e4cb3133d183d4f274524f0b66c621
MehrabR <mehrab.rahman@revature.com>
2018-05-13 17:58:00 -0400

    v0.2

--- jenkins-aws.sh
+++ jenkins-aws.sh
@@ -6,12 +6,9 @@ sudo yum remove -y java
 sudo yum install -y java-1.8.0-openjdk-devel
 
 echo "installing tomcat"
-sudo yum install -y tomcat8 tomcat8-webapps tomcat8-admin-webapps tomcat8-docs-webapp
-sudo sed "$d" /usr/share/tomcat8/conf/tomcat-users.xml
-echo "  <user username=\"tomcat\" password=\"tomcat\" roles=\"manager-script\" />" >> /usr/share/tomcat8/conf/tomcat-users.xml
-echo "</tomcat-users>" >> /usr/share/tomcat8/conf/tomcat-users.xml
-sudo chmod 777 -R /usr/share/tomcat8
-sudo sed -i 's/<Connector port="8080"/<Connector port="80"/' /usr/share/tomcat8/conf/server.xml
+sudo yum install -y tomcat8 tomcat8-webapps tomcat8-admin-webapps
+sudo sed -i 's/<\/tomcat-users>/  <user username=\"tomcat\" password=\"tomcat\" roles=\"manager-gui\" \/>\n<\/tomcat-users>/g' /usr/share/tomcat8/conf/tomcat-users.xml
+sudo sed -i 's/<Connector port=\"8080\"/<Connector port=\"8888\"/' /usr/share/tomcat8/conf/server.xml
 sudo service tomcat8 start
 export CATALINA_HOME=/usr/share/tomcat8
 
@@ -23,6 +20,6 @@ sudo service jenkins start
 
 echo "installing docker"
 sudo yum install -y docker
-sudo groupadd docker
+sudo service docker start
 sudo usermod -aG docker jenkins
 sudo service jenkins restart
